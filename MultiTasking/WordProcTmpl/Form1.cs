5cacf5ba55c23ecf0c3c1474eae8a5f48509e189
Manuel Greiler <manuel.greiler@gmail.com>
2023-03-14 16:51:37 +0100

    added Videos_HL_1.docx and updated Word Processor

--- MultiTasking/WordProcTmpl/Form1.cs
+++ MultiTasking/WordProcTmpl/Form1.cs
@@ -1,62 +1,58 @@
 ﻿using System;
 // using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 // using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace WordProc
 {
-  public delegate void FuncType1(int aData);
-  
-  public partial class Form1 : Form
-  {
-    public static Form1 frm;
-    FuncType1 WorkerMessageFuncPtr;
-    WordProc wrp;
-    
-    public Form1()
-    {
-      InitializeComponent();
-      WorkerMessageFuncPtr = this.RecvMessageFromWorker;
-      frm = this;
-    }
+    public delegate void FuncType1(int aData);
 
-    protected override void OnLoad(EventArgs e)
+    public partial class Form1 : Form
     {
-      wrp = new WordProc();
-      base.OnLoad(e);
-    }
+        public static Form1 frm;
+        FuncType1 WorkerMessageFuncPtr;
+        WordProc wrp;
 
-    protected override void OnFormClosing(FormClosingEventArgs e)
-    {
-      // wrp.thr.Abort();
-    }
+        public Form1()
+        {
+            InitializeComponent();
+            WorkerMessageFuncPtr = this.RecvMessageFromWorker;
+            frm = this;
+        }
 
-    void OnMenueStartWordProc(object sender, EventArgs e)
-    {
-      // An den Worker Text übergeben und
-      // dem Worker die Start-Nachricht schicken
-      // _txtBox1.Text;
-      wrp.inTxt = _txtBox1.Text;
-      wrp.mbx.Put(1);
-    }
+        protected override void OnLoad(EventArgs e)
+        {
+            wrp = new WordProc();
+            base.OnLoad(e);
+        }
 
-    // wird im Context des GUI-Threads aufgerufen
-    public void RecvMessageFromWorker(int aData)
-    {
-      // wird vom GUI-Thread aufgerufen wenn eine
-      // Nachricht mit SendMessage2Form eingequeued wurde
-    }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // wrp.thr.Abort();
+        }
 
-    // wird im Context des WorkerThreads aufgerufen
-    public void SendMessage2Form(int aData)
-    {
-      // wird vom Worker aufgerufen um die Form
-      // über die Msg-Queue des GUI-Threads zu benachrichtigen
-      // Hier gehört das mit dem Begin-Invoke rein
+        void OnMenueStartWordProc(object sender, EventArgs e)
+        {
+            // An den Worker Text übergeben und
+            // dem Worker die Start-Nachricht schicken
+            // _txtBox1.Text;
+            wrp.inTxt = _txtBox1.Text;
+            wrp.mbx.Put(1);
+        }
+
+        // wird im Context des GUI-Threads aufgerufen
+        public void RecvMessageFromWorker(int aData)
+        {
+            // wird vom GUI-Thread aufgerufen wenn eine
+            // Nachricht mit SendMessage2Form eingequeued wurde
+        }
+
+        // wird im Context des WorkerThreads aufgerufen
+        public void SendMessage2Form(int aData)
+        {
+            // wird vom Worker aufgerufen um die Form
+            // über die Msg-Queue des GUI-Threads zu benachrichtigen
+            // Hier gehört das mit dem Begin-Invoke rein
+        }
     }
-  }
 }
--- MultiTasking/WordProcTmpl/WordProc.cs
+++ MultiTasking/WordProcTmpl/WordProc.cs
@@ -1,51 +1,50 @@
-﻿using System;
-// using System.Collections.Generic;
+﻿// using System.Collections.Generic;
 // using System.Linq;
 using System.Text;
 using System.Threading;
 
 namespace WordProc
 {
-  // Dieselbe MailBox-Idee wie im Request/Response Beispiel
-  class MailBox
-  {
-    int msg;
-    public void Put(int aMsg) // Signals waiting Thread
+    // Dieselbe MailBox-Idee wie im Request/Response Beispiel
+    class MailBox
     {
-    }
-    public int Get() // Blocks Caller
-    {
-      return 0;
-    }
-  }
-
-  class WordProc
-  {
-    public Thread thr;
-    public string inTxt; // der zu verarbeitende Input-Text
-    
-    // der word-processte output-Text
-    public StringBuilder outTxt = new StringBuilder(2000);
-    
-    public MailBox mbx = new MailBox(); // Mailbox of Worker
-
-    public WordProc()
-    {
-      // thr = new Thread(MainLoop);
-      // thr.Start();
+        int msg;
+        public void Put(int aMsg) // Signals waiting Thread
+        {
+        }
+        public int Get() // Blocks Caller
+        {
+            return 0;
+        }
     }
 
-    void MainLoop()
+    class WordProc
     {
-      // an der mbx auf einen neuen Auftrag warten
-      // wenn neuer Autrag da ProcessWords() aufrufen
-    }
+        public Thread thr;
+        public string inTxt; // der zu verarbeitende Input-Text
 
-    void ProcessWords()
-    {
-      // Den inTxt char für char word-processen inTxt[i]
-      // Nach jeweils 20 Buchstabe die Form benachrichtigen
-      // damit das momentane Zwischenergebniss angezeigt werden kann
+        // der word-processte output-Text
+        public StringBuilder outTxt = new StringBuilder(2000);
+
+        public MailBox mbx = new MailBox(); // Mailbox of Worker
+
+        public WordProc()
+        {
+            // thr = new Thread(MainLoop);
+            // thr.Start();
+        }
+
+        void MainLoop()
+        {
+            // an der mbx auf einen neuen Auftrag warten
+            // wenn neuer Autrag da ProcessWords() aufrufen
+        }
+
+        void ProcessWords()
+        {
+            // Den inTxt char für char word-processen inTxt[i]
+            // Nach jeweils 20 Buchstabe die Form benachrichtigen
+            // damit das momentane Zwischenergebniss angezeigt werden kann
+        }
     }
-  }
 }
